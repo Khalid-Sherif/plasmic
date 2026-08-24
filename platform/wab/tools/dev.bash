9049b0b24136f7725f58a86c3812bb2295040db2
Khaled Sherif <79546733+Khalid-Sherif@users.noreply.github.com>
2026-08-24 15:42:37 +0300

    Update dev.bash

--- platform/wab/tools/dev.bash
+++ platform/wab/tools/dev.bash
@@ -1,15 +1,12 @@
 main() {
   concurrently \
   --names frontend,host,css,sub,canvas,react-web,live-frame,loader-html,backend \
-<<<<<<< HEAD
   "PUBLIC_URL=$PUBLIC_URL TRANSPILER=swc nice -n +30 yarn start" \
   'yarn host-server' \
   'nice -n +30 yarn watch-css' \
-=======
   'TRANSPILER=swc nice -n +30 pnpm start' \
   'pnpm host-server' \
   'nice -n +30 pnpm watch-css' \
->>>>>>> upstream/master
   'cd ../sub/; yarn; nice -n +30 yarn watch' \
   'cd ../canvas-packages/; nice -n +30 pnpm watch' \
   'cd ../react-web-bundle/; yarn; nice -n +30 yarn watch' \
