60b39d5e315548d0dffde7cc9e33c7684cacfff0
Khalid-Sherif <khsherif13@gmail.com>
2026-08-29 07:53:20 +0300

    (fix): change site asset bucket value to take whatever has been given in the server

--- platform/wab/tools/backend-server.bash
+++ platform/wab/tools/backend-server.bash
@@ -5,7 +5,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/host-url.bash"
 REACT_APP_DEFAULT_HOST_URL=${HOST_URL} \
         REACT_APP_PUBLIC_URL=${REACT_APP_DEV_PROXY:-${REACT_APP_PUBLIC_URL:-http://localhost:3003}} \
         CODEGEN_HOST=${REACT_APP_DEV_PROXY:-${CODEGEN_HOST:-http://localhost:3003}} \
-        SITE_ASSETS_BUCKET=plasmic-site-assets \
-        SITE_ASSETS_BASE_URL='https://site-assets.plasmic.app/' \
+        SITE_ASSETS_BUCKET=${SITE_ASSETS_BUCKET:-plasmic-site-assets} \
+        SITE_ASSETS_BASE_URL=${SITE_ASSETS_BASE_URL:-'https://site-assets.plasmic.app/'} \
         DISABLE_BWRAP=1 \
         bash tools/run.bash src/wab/server/main.ts
